51ec55474139bda7cc6438f74518a2e58c6613e4
Philippe Teuwen <phil@teuwen.org>
2016-06-23 00:40:30 +0200

    Adding helpers

---
+++ helpers/README.md
@@ -0,0 +1,3 @@
+# Helpers
+
+Helpers are specific for automating a set of scans but can probably be reusable after some tuning on other sources as well.
---
+++ helpers/process_archive_atari.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+pdf=$1
+# which pages contain databar?
+firstpage=$2
+lastpage=$3
+
+echo "Extracting pages $firstpage to $lastpage from $pdf..."
+pdftk $pdf cat $firstpage-$lastpage output $pdf.$firstpage-$lastpage.pdf
+pdfimages -all $pdf.$firstpage-$lastpage.pdf databar
+
+ytop=220
+ybottom=3040
+xleft=120
+xright=2410
+for i in databar-*.png; do
+    echo "Cropping $i, upscaling, thresholding..."
+    mogrify -crop $(($xright-$xleft))x$(($ybottom-$ytop))+$xleft+$ytop +repage -resize 300% -threshold 50% $i
+done
+echo "Oscar..."
+./oscar.py databar-*.png 2> $pdf.$firstpage-$lastpage.log| tee databar.hex
+./post_atari.sh databar.hex
+d=${pdf%.pdf}
+mkdir $d
+mv databar.hex databar.hex.bin $d
+rm databar-*.png
+rm $pdf.$firstpage-$lastpage.pdf
